7540528457ab38db631ffa2bb511f65bad747709
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2023-11-17 11:58:44 -0500

    Enable searching by hugoSymbol

--- src/main/webapp/app/config/constants/constants.ts
+++ src/main/webapp/app/config/constants/constants.ts
@@ -9,6 +9,7 @@ export const messages = {
 };
 
 export const APP_DATE_FORMAT = 'DD/MM/YY HH:mm';
+export const APP_DATETIME_FORMAT = 'MM/DD/YYYY h:mm A';
 export const APP_TIMESTAMP_FORMAT = 'DD/MM/YY HH:mm:ss';
 export const APP_LOCAL_DATE_FORMAT = 'DD/MM/YYYY';
 export const APP_LOCAL_DATETIME_FORMAT = 'YYYY-MM-DDTHH:mm';
