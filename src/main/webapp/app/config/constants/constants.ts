61a5d81c088bbc5e593b920dde29f4fc19be3609
Benjamin Preiser <bprize15@gmail.com>
2023-12-05 08:38:34 -0500

    add timeseries component
    finish timeline functionality
    
    add timeseries
    
    revert gene list page
    
    update line height calculation
    
    update yarn.lock
    
    add time series format
    
    fix test
    
    update css
    
    remove extra sort from groupTimeSeriesDataByDay

--- src/main/webapp/app/config/constants/constants.ts
+++ src/main/webapp/app/config/constants/constants.ts
@@ -15,6 +15,8 @@ export const APP_DATETIME_FORMAT = 'MM/DD/YYYY h:mm A';
 export const APP_LOCAL_DATETIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 export const APP_WHOLE_NUMBER_FORMAT = '0,0';
 export const APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT = '0,0.[00]';
+export const APP_HISTORY_FORMAT = 'MMM, D YYYY';
+export const APP_TIME_FORMAT = 'h:mm A';
 
 export const REDIRECT_TIMEOUT_MILLISECONDS = 10000;
 export const SHORT_REDIRECT = 2000;
