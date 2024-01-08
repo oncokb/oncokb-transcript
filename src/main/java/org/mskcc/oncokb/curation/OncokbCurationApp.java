b160cc219491a7ae111c00ae1c3a89b2f222516a
Hongxin <5400599+zhx828@users.noreply.github.com>
2024-01-08 17:01:40 -0500

    Update OncokbCurationApp.java

--- src/main/java/org/mskcc/oncokb/curation/OncokbCurationApp.java
+++ src/main/java/org/mskcc/oncokb/curation/OncokbCurationApp.java
@@ -91,7 +91,6 @@ public class OncokbCurationApp {
             env.getActiveProfiles().length == 0 ? env.getDefaultProfiles() : env.getActiveProfiles()
         );
         if (activeProfiles.contains("importer")) {
-            initFirebase();
             importer.generalImport();
         }
     }
