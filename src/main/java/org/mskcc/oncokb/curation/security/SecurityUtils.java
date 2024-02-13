5e398edaa862ad65cf5c842afe30a7f7d8fedfd3
Hongxin <5400599+zhx828@users.noreply.github.com>
2024-02-13 09:44:11 -0600

    Update SecurityUtils.java

--- src/main/java/org/mskcc/oncokb/curation/security/SecurityUtils.java
+++ src/main/java/org/mskcc/oncokb/curation/security/SecurityUtils.java
@@ -9,10 +9,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 /**
