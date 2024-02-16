5be1baaa3eb793a9aee2eeb075a5a4977e47698d
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-02-16 14:24:54 -0500

    Update mutation stats to not show oncogenicity

--- src/main/webapp/app/pages/curation/nestLevelSummary/MutationLevelSummary.tsx
+++ src/main/webapp/app/pages/curation/nestLevelSummary/MutationLevelSummary.tsx
@@ -1,19 +1,19 @@
 import { componentInject } from 'app/shared/util/typed-inject';
 import { IRootStore } from 'app/stores';
-import { AllLevelSummary, MutationLevelSummary } from 'app/stores/firebase/firebase.gene.store';
+import { MutationLevelSummary } from 'app/stores/firebase/firebase.gene.store';
 import _ from 'lodash';
 import { observer } from 'mobx-react';
-import React, { useMemo } from 'react';
+import React from 'react';
 import NestLevelSummary from './NestLevelSummary';
-import { ONCOGENICITY } from 'app/config/constants/constants';
 
 export interface MutationLevelSummaryProps extends StoreProps {
   mutationUuid: string;
+  hideOncogenicity?: boolean;
 }
 
-const MutationLevelSummary = (props: MutationLevelSummaryProps) => {
-  const mutationLevelSummary = props.mutationSummaryStats[props.mutationUuid];
-  return <NestLevelSummary summaryStats={mutationLevelSummary} />;
+const MutationLevelSummary = ({ mutationSummaryStats, mutationUuid, hideOncogenicity = false }: MutationLevelSummaryProps) => {
+  const mutationLevelSummary = mutationSummaryStats[mutationUuid];
+  return <NestLevelSummary summaryStats={mutationLevelSummary} hideOncogenicity={hideOncogenicity} />;
 };
 
 const mapStoreToProps = ({ firebaseGeneStore }: IRootStore) => ({
--- src/main/webapp/app/pages/curation/nestLevelSummary/NestLevelSummary.tsx
+++ src/main/webapp/app/pages/curation/nestLevelSummary/NestLevelSummary.tsx
@@ -22,18 +22,19 @@ export type NestLevelSummaryStats = {
 
 export interface NestLevelSummaryProps {
   summaryStats: NestLevelSummaryStats;
+  hideOncogenicity?: boolean;
   isTreatmentStats?: boolean;
 }
 
-export const NestLevelSummary = (props: NestLevelSummaryProps) => {
+export const NestLevelSummary = ({ summaryStats, hideOncogenicity, isTreatmentStats }: NestLevelSummaryProps) => {
   const summaryKeys: (keyof NestLevelSummaryStats)[] = ['TTS', 'DxS', 'PxS'];
 
   let lastBadgeHasHiddenNumber = false;
 
-  const badges = props.summaryStats ? (
+  const badges = summaryStats ? (
     <>
-      {Object.keys(props.summaryStats)
-        .filter(k => props.summaryStats[k] && props.summaryStats[k] > 0)
+      {Object.keys(summaryStats)
+        .filter(k => summaryStats[k] && summaryStats[k] > 0)
         .map(k => {
           const hideWhenOne = summaryKeys.includes(k as keyof NestLevelSummaryStats);
           if (hideWhenOne) {
@@ -41,61 +42,61 @@ export const NestLevelSummary = (props: NestLevelSummaryProps) => {
           }
           return (
             <CountBadge
-              count={props.summaryStats[k]}
+              count={summaryStats[k]}
               base={k}
               key={`summaries-${k}`}
               hideWhenOne={summaryKeys.includes(k as keyof NestLevelSummaryStats)}
             />
           );
         })}
-      {Object.keys(props.summaryStats.txLevels)
+      {Object.keys(summaryStats.txLevels)
         .filter(k => k !== TX_LEVELS.LEVEL_NO)
         .sort(sortByTxLevel)
         .map(k => {
           lastBadgeHasHiddenNumber = false;
-          return props.isTreatmentStats ? (
+          return isTreatmentStats ? (
             <div key={`tx-levels-${k}`} className="count-badge-wrapper all-children-margin">
               <span className={classNames('oncokb', 'icon', `level-${k}`)}></span>
             </div>
           ) : (
             <CountBadge
-              count={props.summaryStats.txLevels[k]}
+              count={summaryStats.txLevels[k]}
               base={<span className={classNames('oncokb', 'icon', `level-${k}`)}></span>}
               key={`tx-levels-${k}`}
             />
           );
         })}
-      {Object.keys(props.summaryStats.dxLevels)
+      {Object.keys(summaryStats.dxLevels)
         .sort()
         .map(k => {
           lastBadgeHasHiddenNumber = false;
           return (
             <CountBadge
-              count={props.summaryStats.dxLevels[k]}
+              count={summaryStats.dxLevels[k]}
               base={<span className={classNames('oncokb', 'icon', `level-${k}`)}></span>}
               key={`dx-levels-${k}`}
             />
           );
         })}
-      {Object.keys(props.summaryStats.pxLevels)
+      {Object.keys(summaryStats.pxLevels)
         .sort()
         .map(k => {
           lastBadgeHasHiddenNumber = false;
           return (
             <CountBadge
-              count={props.summaryStats.pxLevels[k]}
+              count={summaryStats.pxLevels[k]}
               base={<span className={classNames('oncokb', 'icon', `level-${k}`)}></span>}
               key={`px-levels-${k}`}
             />
           );
         })}
-      {props.summaryStats.oncogenicity ? (
+      {!hideOncogenicity && summaryStats.oncogenicity ? (
         <CountBadge
           hideWhenOne
           count={1}
           base={
-            <DefaultTooltip placement="top" overlay={<span>{FIREBASE_ONCOGENICITY_MAPPING[props.summaryStats.oncogenicity]}</span>}>
-              <span className={classNames('oncokb', 'icon', `${ONCOGENICITY_CLASS_MAPPING[props.summaryStats.oncogenicity]}`)}></span>
+            <DefaultTooltip placement="top" overlay={<span>{FIREBASE_ONCOGENICITY_MAPPING[summaryStats.oncogenicity]}</span>}>
+              <span className={classNames('oncokb', 'icon', `${ONCOGENICITY_CLASS_MAPPING[summaryStats.oncogenicity]}`)}></span>
             </DefaultTooltip>
           }
         />
@@ -105,11 +106,11 @@ export const NestLevelSummary = (props: NestLevelSummaryProps) => {
     <></>
   );
 
-  if (props.summaryStats?.oncogenicity) {
+  if (summaryStats?.oncogenicity) {
     lastBadgeHasHiddenNumber = true;
   }
 
-  if (props.summaryStats) {
+  if (summaryStats) {
     return <div className={classNames('d-flex align-items-center all-children-margin')}>{badges}</div>;
   }
   return <div></div>;
