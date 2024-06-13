0467cdb01a1d415f7a843395e38cbfe0a075965e
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2024-06-13 13:50:09 -0400

    remove comment

--- src/main/webapp/app/config/constants/regex.ts
+++ src/main/webapp/app/config/constants/regex.ts
@@ -9,5 +9,3 @@ export const UUID_REGEX = new RegExp('\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}');
 export const WHOLE_NUMBER_REGEX = new RegExp('^\\d+$');
 
 export const INTEGER_REGEX = /^-?\d+$/;
-
-// export const REFERENCE_LINK_REGEX = /(\(\s*(?:PMID|NCT|Abstract):?.*?\))/i;
