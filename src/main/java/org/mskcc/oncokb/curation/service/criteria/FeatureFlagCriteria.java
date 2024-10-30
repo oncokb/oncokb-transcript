7dd08a3abd76dbd90ad58bb925594090c014f6e1
gebingbing <ykg5143@psu.edu>
2024-10-30 17:40:16 -0400

    Add Feature Flag by Jhipster

---
+++ src/main/java/org/mskcc/oncokb/curation/service/criteria/FeatureFlagCriteria.java
@@ -0,0 +1,174 @@
+package org.mskcc.oncokb.curation.service.criteria;
+
+import java.io.Serializable;
+import java.util.Objects;
+import tech.jhipster.service.Criteria;
+import tech.jhipster.service.filter.BooleanFilter;
+import tech.jhipster.service.filter.DoubleFilter;
+import tech.jhipster.service.filter.Filter;
+import tech.jhipster.service.filter.FloatFilter;
+import tech.jhipster.service.filter.IntegerFilter;
+import tech.jhipster.service.filter.LongFilter;
+import tech.jhipster.service.filter.StringFilter;
+
+/**
+ * Criteria class for the {@link org.mskcc.oncokb.curation.domain.FeatureFlag} entity. This class is used
+ * in {@link org.mskcc.oncokb.curation.web.rest.FeatureFlagResource} to receive all the possible filtering options from
+ * the Http GET request parameters.
+ * For example the following could be a valid request:
+ * {@code /feature-flags?id.greaterThan=5&attr1.contains=something&attr2.specified=false}
+ * As Spring is unable to properly convert the types, unless specific {@link Filter} class are used, we need to use
+ * fix type specific filters.
+ */
+public class FeatureFlagCriteria implements Serializable, Criteria {
+
+    private static final long serialVersionUID = 1L;
+
+    private LongFilter id;
+
+    private StringFilter name;
+
+    private StringFilter description;
+
+    private BooleanFilter enabled;
+
+    private LongFilter userId;
+
+    private Boolean distinct;
+
+    public FeatureFlagCriteria() {}
+
+    public FeatureFlagCriteria(FeatureFlagCriteria other) {
+        this.id = other.id == null ? null : other.id.copy();
+        this.name = other.name == null ? null : other.name.copy();
+        this.description = other.description == null ? null : other.description.copy();
+        this.enabled = other.enabled == null ? null : other.enabled.copy();
+        this.userId = other.userId == null ? null : other.userId.copy();
+        this.distinct = other.distinct;
+    }
+
+    @Override
+    public FeatureFlagCriteria copy() {
+        return new FeatureFlagCriteria(this);
+    }
+
+    public LongFilter getId() {
+        return id;
+    }
+
+    public LongFilter id() {
+        if (id == null) {
+            id = new LongFilter();
+        }
+        return id;
+    }
+
+    public void setId(LongFilter id) {
+        this.id = id;
+    }
+
+    public StringFilter getName() {
+        return name;
+    }
+
+    public StringFilter name() {
+        if (name == null) {
+            name = new StringFilter();
+        }
+        return name;
+    }
+
+    public void setName(StringFilter name) {
+        this.name = name;
+    }
+
+    public StringFilter getDescription() {
+        return description;
+    }
+
+    public StringFilter description() {
+        if (description == null) {
+            description = new StringFilter();
+        }
+        return description;
+    }
+
+    public void setDescription(StringFilter description) {
+        this.description = description;
+    }
+
+    public BooleanFilter getEnabled() {
+        return enabled;
+    }
+
+    public BooleanFilter enabled() {
+        if (enabled == null) {
+            enabled = new BooleanFilter();
+        }
+        return enabled;
+    }
+
+    public void setEnabled(BooleanFilter enabled) {
+        this.enabled = enabled;
+    }
+
+    public LongFilter getUserId() {
+        return userId;
+    }
+
+    public LongFilter userId() {
+        if (userId == null) {
+            userId = new LongFilter();
+        }
+        return userId;
+    }
+
+    public void setUserId(LongFilter userId) {
+        this.userId = userId;
+    }
+
+    public Boolean getDistinct() {
+        return distinct;
+    }
+
+    public void setDistinct(Boolean distinct) {
+        this.distinct = distinct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final FeatureFlagCriteria that = (FeatureFlagCriteria) o;
+        return (
+            Objects.equals(id, that.id) &&
+            Objects.equals(name, that.name) &&
+            Objects.equals(description, that.description) &&
+            Objects.equals(enabled, that.enabled) &&
+            Objects.equals(userId, that.userId) &&
+            Objects.equals(distinct, that.distinct)
+        );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, enabled, userId, distinct);
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "FeatureFlagCriteria{" +
+            (id != null ? "id=" + id + ", " : "") +
+            (name != null ? "name=" + name + ", " : "") +
+            (description != null ? "description=" + description + ", " : "") +
+            (enabled != null ? "enabled=" + enabled + ", " : "") +
+            (userId != null ? "userId=" + userId + ", " : "") +
+            (distinct != null ? "distinct=" + distinct + ", " : "") +
+            "}";
+    }
+}
