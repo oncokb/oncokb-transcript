c8100c10a62f7da2cf19ebb8d9e72e3fa658de04
gebingbing <ykg5143@psu.edu>
2025-01-10 13:50:33 -0500

    use DEFAULT_ICON_SIZE

--- src/main/webapp/app/pages/curation/header/ReadOnlyBanner.tsx
+++ src/main/webapp/app/pages/curation/header/ReadOnlyBanner.tsx
@@ -6,6 +6,7 @@ import { IRootStore } from 'app/stores';
 import { onValue, ref } from 'firebase/database';
 import { getFirebaseMetaGenePath } from 'app/shared/util/firebase/firebase-utils';
 import { BiInfoCircle } from 'react-icons/bi';
+import { DEFAULT_ICON_SIZE } from 'app/config/constants/constants';
 
 export interface IReadOnlyBanner extends StoreProps {
   hugoSymbol: string;
@@ -29,7 +30,7 @@ const ReadOnlyBanner = ({ isGermline, hugoSymbol, firebaseDb }: IReadOnlyBanner)
 
   return meta ? (
     <div className="alert alert-primary alert-dismissible fade show d-flex align-items-center" role="alert">
-      <BiInfoCircle size={25} className="me-2" />
+      <BiInfoCircle size={DEFAULT_ICON_SIZE} className="me-2" />
       <span>
         {isGermline ? 'The germline ' : 'The somatic '}
         section is currently in read only mode because {meta.review.currentReviewer} is currently reviewing.
