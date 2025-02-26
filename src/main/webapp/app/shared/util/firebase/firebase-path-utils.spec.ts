3f3da1f719093470337528ac87f4aff6137cd2fe
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2025-02-26 11:02:26 -0500

    Fix two unit tests

--- src/main/webapp/app/shared/util/firebase/firebase-path-utils.spec.ts
+++ src/main/webapp/app/shared/util/firebase/firebase-path-utils.spec.ts
@@ -4,11 +4,11 @@ import { buildFirebaseGenePath, extractArrayPath, parseFirebaseGenePath } from '
 describe('FirebasePathUtils', () => {
   describe('parseFirebaseGenePath', () => {
     it('Should parse path', () => {
-      const path = `${FB_COLLECTION.GENES}/BRAF/mutations/0`;
+      const path = `${FB_COLLECTION.GENES}/BRAF/mutations/-mKey`;
       const pathDetails = parseFirebaseGenePath(path);
       expect(pathDetails?.fullPath).toEqual(path);
       expect(pathDetails?.hugoSymbol).toEqual('BRAF');
-      expect(pathDetails?.pathFromGene).toEqual('mutations/0');
+      expect(pathDetails?.pathFromGene).toEqual('mutations/-mKey');
     });
     it('Should not parse path with less than 3 parts', () => {
       let path = `${FB_COLLECTION.GENES}`;
@@ -25,8 +25,10 @@ describe('FirebasePathUtils', () => {
   describe('buildFirebaseGenePath', () => {
     it('should return firebase path', () => {
       const hugoSymbol = 'BRAF';
-      expect(buildFirebaseGenePath(hugoSymbol, 'mutations/0/name')).toEqual('Genes/BRAF/mutations/0/name');
-      expect(buildFirebaseGenePath(hugoSymbol, 'mutations/0/tumors/0/cancerTypes')).toEqual('Genes/BRAF/mutations/0/tumors/0/cancerTypes');
+      expect(buildFirebaseGenePath(hugoSymbol, 'mutations/-mKey/name')).toEqual('Genes/BRAF/mutations/-mKey/name');
+      expect(buildFirebaseGenePath(hugoSymbol, 'mutations/-mKey/tumors/-tKey/cancerTypes')).toEqual(
+        'Genes/BRAF/mutations/-mKey/tumors/-tKey/cancerTypes',
+      );
 
       expect(buildFirebaseGenePath('', 'mutations_uuid')).toEqual(undefined);
     });
@@ -34,11 +36,15 @@ describe('FirebasePathUtils', () => {
 
   describe('extractArrayPath', () => {
     test.each([
-      { path: 'mutations/0/name', firebaseArrayPath: 'mutations', deleteIndex: 0 },
-      { path: 'mutations/0/tumors/10/cancerTypes', firebaseArrayPath: 'mutations/0/tumors', deleteIndex: 10 },
-      { path: 'mutations/0/tumors/0/TIs/0/treatments/0/name', firebaseArrayPath: 'mutations/0/tumors/0/TIs/0/treatments', deleteIndex: 0 },
-    ])('should return arrayPath = $arrayPath and index = $index when given $path', ({ path, firebaseArrayPath, deleteIndex }) => {
-      expect(extractArrayPath(path)).toEqual({ firebaseArrayPath, deleteIndex });
+      { path: 'mutations/-mKey/name', firebaseArrayPath: 'mutations', deleteArrayKey: '-mKey' },
+      { path: 'mutations/-mKey/tumors/-tKey/cancerTypes', firebaseArrayPath: 'mutations/-mKey/tumors', deleteArrayKey: '-tKey' },
+      {
+        path: 'mutations/-mKey/tumors/-tKey/TIs/0/treatments/-txKey/name',
+        firebaseArrayPath: 'mutations/-mKey/tumors/-tKey/TIs/0/treatments',
+        deleteArrayKey: '-txKey',
+      },
+    ])('should return arrayPath = $arrayPath and index = $index when given $path', ({ path, firebaseArrayPath, deleteArrayKey }) => {
+      expect(extractArrayPath(path)).toEqual({ firebaseArrayPath, deleteArrayKey });
     });
   });
 });
