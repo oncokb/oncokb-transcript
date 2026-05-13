bfa90a656a97339bbdc78a599dfefb241af9839d
Calvin Lu <59149377+calvinlu3@users.noreply.github.com>
2026-05-13 11:48:36 -0400

    revert

--- src/main/webapp/app/shared/table/GenomicIndicatorsTable.tsx
+++ src/main/webapp/app/shared/table/GenomicIndicatorsTable.tsx
@@ -190,6 +190,7 @@ const GenomicIndicatorsTable = ({
             buildCell={genomicIndicator => {
               return (
                 <RealtimeRichTextEditor
+                  style={{ height: '60px' }}
                   firebasePath={`${genomicIndicatorsPath}/${cell.original.arrayKey}/description`}
                   label=""
                   disabled={genomicIndicator.name_review?.removed || readOnly || false}
